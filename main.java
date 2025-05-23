cd7abf8dd0dff1e7eb727d1802d48e75fc16b57d
Aslı Nur <aslinurmavili16500@gmail.com>
2025-05-23 23:45:36 +0300

    Aritmetik işlemler + kullanıcıdan veri alma + if-else kullanımı

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
 
@@ -18,5 +20,35 @@ public class main {
         System.out.println("Mezun mu?: " + mezunMu);
         System.out.println("Cinsiyet: " + cinsiyet);
         System.out.println("Pi sayısı (sabit): " + PI);
+
+
+
+
+
+        //Aritmetik işlemler + kullanıcıdan veri alma + if-else kullanımı
+        Scanner input = new Scanner(System.in);
+        System.out.println("Birinci sayıyı girin: ");
+        double sayi1 = input.nextDouble(); //nextDouble() ile kullanıcıdan ondalıklı veya tam sayılar alınabilir.
+
+        System.out.println("İkinci sayıyı girin: ");
+        double sayi2 = input.nextDouble();
+
+        double toplam = sayi1 + sayi2;
+        double fark = sayi1 - sayi2;
+        double carpim = sayi1 * sayi2;
+        double bolum = sayi2 != 0 ? sayi1 / sayi2 : 0; // Sıfıra bölme kontrolü
+
+        System.out.println("\n--- İşlem Sonuçları ---");
+        System.out.println("Toplam: " + toplam);
+        System.out.println("Fark: " + fark);
+        System.out.println("Çarpım: " + carpim);
+
+        if (sayi2 != 0) {
+            System.out.println("Bölüm: " + bolum);
+        } else {
+            System.out.println("Bölüm: Sıfıra bölme hatası!");
+        }
+
+        input.close();
     }
 }
